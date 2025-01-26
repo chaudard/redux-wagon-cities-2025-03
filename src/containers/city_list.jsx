e1147a6ee2923c7a84c226a773ab71bee74f54eb
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2025-01-26 23:38:27 +0100

    create Citylist container and populate it

---
+++ src/containers/city_list.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class CityList extends Component {
+    constructor (props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+}
+
+export default CityList;
